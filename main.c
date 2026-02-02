9cd7dba11274732327fbf06f0d6bae3fcefa20bd
shayne2000 <126381922+Shayne2000@users.noreply.github.com>
2026-02-02 09:20:33 +0000

    push1

--- main.c
+++ main.c
@@ -9,29 +9,57 @@
 #include <stdlib.h>
 #include "node.h"
 
+void show (struct node z) {
+    struct node *test ;
+    test = &z ;
+    while (test != NULL){
+        printf("%d\n",test->value);
+        test = test->next ;
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    int c=5;
-    struct node a,b,*head ;
-    a.value = c;
+    int x=5;
+    struct node a,b,c,d,*head ;
+    a.value = x;
     a.next=&b;
     head=&a;
     b.value=head->value+3;
 
-    printf("%d\n", head ->value ); //what value for 5
-    printf("%d\n", head ->next->value ); //what value for 8
+    // printf("%d\n", head ->value ); //what value for 5
+    // printf("%d\n", head ->next->value ); //what value for 8
 /*  Exercise I
     1. Add 1 more than at the end
     2. Add value(11)
     3. Make next become NULL
  */
+
+b.next = &c ;
+c.value = 11 ;
+c.next = NULL ;
+
     
 /*  Exercise II
         1. Add 1 more than at the begining!!!!
         2. Add value (2)
         
 */
+struct node z ;
+z.value = 2 ;
+z.next = &a ;
+
+
+struct node y ;
+a.next = &y ;
+y.next = &b ;
+y.value = 123 ;
+
+// show(z);
+    
+
+
     typedef struct node* NodePtr;
-    NodePtr tmp=head; //add temp value to faciliate
+    NodePtr tmp=&z; //add temp value to faciliate
         
     /*  Exercise III Use loop to print everything
         int i,n=5;
@@ -40,6 +68,11 @@ int main(int argc, const char * argv[]) {
           // What is missing???
         }
     */
+    // int i,n=5;
+    // for(i=0;i<n;i++){
+    //     printf("%5d", tmp->value);
+    //     tmp = tmp->next;
+    // }
     
    /*  Exercise IV change to while loop!! (you can use NULL to help)
        
@@ -48,16 +81,58 @@ int main(int argc, const char * argv[]) {
            // What is missing???
         }
     */
+    // while(tmp != NULL){
+    //     printf("%5d", tmp->value);
+    //     tmp = tmp->next ;
+    // }
     
  /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
          //use a loop to help
           
      */
+    int n = 10 ;
+    struct node *p,*h ;
+    h = (struct node*)malloc(sizeof(struct node));
+
+    p = h ;
+    
+    for (int i = 0 ; i < n-1 ; i ++) {
+        
+        p->value = i ;
+
+        p->next = (struct node*)malloc(sizeof(struct node));
+
+        p = p->next ;
+    }p->value = 55 ;
+    show(*h);
+
+
+
+
 
     /*  Exercise VI Free all node !!
          //use a loop to help
           
      */
+
+    p = h ;
+    for (int i = n ; i >= 0 ; i --){
+        p = h ;
+        for (int j = 0 ; j < i ; j ++) {
+            p = p->next ;
+        }
+        free(p);
+    }
+
+    // show(*h);
+
+
     
+
+
+
+
+
+
     return 0;
 }
